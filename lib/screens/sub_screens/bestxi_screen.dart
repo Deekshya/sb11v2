e838b074c1008b5091948fcb09c5f2cf3ea2d451
Deekshya <deekshyamoharana@gmail.com>
2021-10-11 08:34:39 +0530

    image coming soon done

--- lib/screens/sub_screens/bestxi_screen.dart
+++ lib/screens/sub_screens/bestxi_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:sports_buzz11_trial1/constants.dart';
 
 class BestXIScreen extends StatelessWidget {
@@ -6,15 +8,42 @@ class BestXIScreen extends StatelessWidget {
 
   BestXIScreen({required this.cardIndex});
 
+  List<String> best11Title = ['Grand League', 'Small League', 'Head to Head'];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: best11ImageUrl[cardIndex].length,
         itemBuilder: (context, index) {
           return Container(
-            child: Image(
-                image:
-                    NetworkImage(best11ImageUrl[cardIndex][index].toString())),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Team ' + '${index + 1}: ${best11Title[index]}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline,
+                        decorationThickness: 2),
+                  ),
+                ),
+                Stack(
+                  alignment: AlignmentDirectional.center,
+                  children: [
+                    Text(
+                      'Coming Soon...',
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image(
+                          image: NetworkImage(
+                        best11ImageUrl[cardIndex][index].toString(),
+                      )),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           );
         });
   }
